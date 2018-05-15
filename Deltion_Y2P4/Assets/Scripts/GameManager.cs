7f59875d04ba82c16aba68cd8598e6cf9ef184f7
Kevin Otten <kevin.otten@ziggo.nl>
2018-05-15 16:39:56 +0200

    updateje

---
+++ Deltion_Y2P4/Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance;
+
+	void Start()
+	{
+		if(instance == null)
+		{
+			instance = this;
+		}
+		else if(instance != this)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Deltion_Y2P4/Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsManager : MonoBehaviour {
+
+	public static SettingsManager instance;
+
+	void Start()
+	{
+		if(instance == null)
+		{
+			instance = this;
+		}
+		else if(instance != this)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
