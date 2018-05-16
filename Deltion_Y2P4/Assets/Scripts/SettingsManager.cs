fb092a33daa614ad3eeef1ea286803711146f454
Kevin Otten <kevin.otten@ziggo.nl>
2018-05-16 14:24:34 +0200

    wat basic functies voor het veranderen van volume en quality settings

--- Deltion_Y2P4/Assets/Scripts/SettingsManager.cs
+++ Deltion_Y2P4/Assets/Scripts/SettingsManager.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Audio;
 public class SettingsManager : MonoBehaviour {
 
 	public static SettingsManager instance;
+	public AudioMixer mainMixer;
+
+	public bool screenMode;
 
 	void Start()
 	{
@@ -15,6 +18,40 @@ public class SettingsManager : MonoBehaviour {
 		else if(instance != this)
 		{
 			Destroy(gameObject);
+		}
+	}
+
+	public void GameVolume(float volume)
+	{
+		mainMixer.SetFloat("Master",volume);
+	}
+
+	public void GameVolumeMusic(float volume)
+	{
+		mainMixer.SetFloat("Music",volume);
+	}
+
+	public void GameVolumeEffects(float volume)
+	{
+		mainMixer.SetFloat("SoundEffects",volume);
+	}
+
+	public void ChangeQuality(int qualityIndex)
+	{
+		QualitySettings.SetQualityLevel(qualityIndex);
+	}
+
+	public void ChangeScreenMode(int screenModeIndex)
+	{	
+		if(screenModeIndex == 0)
+		{
+			screenMode = true;
+			Screen.fullScreen = true;
+		}
+		else if(screenModeIndex == 1)
+		{
+			screenMode = false;
+			Screen.fullScreen = false;
 		}
 	}
 }
