c3fc8b5e16b2bd5ae0930b98d70a7a7e2b80a90b
Kevin Otten <kevin.otten@ziggo.nl>
2018-05-22 16:04:53 +0200

    credits screen switch button

---
+++ Deltion_Y2P4/Assets/Kevin/WorkingOn/Settings/Scripts/SettingsCredits.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsCredits : MonoBehaviour {
+
+	public GameObject creditsPannel;
+
+	public GameObject titelPannel;
+	public void ToggleMainScreen()
+	{
+		if(titelPannel.activeSelf == true)
+		{
+			titelPannel.SetActive(false);
+			creditsPannel.SetActive(true);
+		}
+		else if(creditsPannel.activeSelf == true)
+		{
+			creditsPannel.SetActive(true);
+			titelPannel.SetActive(true);
+		}
+	}
+}
