500bef7c4dc703e82829799dfdbfab4f8d8f0f94
Kevin Otten <kevin.otten@ziggo.nl>
2018-05-25 17:14:47 +0200

    update main scene

---
+++ Deltion_Y2P4/Assets/Kevin/WorkingOn/Door.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+	public Animator anim;
+	public void OpenCloseDoor()
+	{
+
+	}
+}
