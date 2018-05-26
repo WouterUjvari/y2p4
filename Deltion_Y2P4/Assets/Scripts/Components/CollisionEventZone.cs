c4fab77712c6bb4d16ad6173912969fccc8ea3d3
Dominik Beens <dominik.beens@hotmail.nl>
2018-05-27 00:01:51 +0200

    some work on the roll the ball puzzle (is finishable now)

---
+++ Deltion_Y2P4/Assets/Scripts/Components/CollisionEventZone.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class CollisionEventZone : MonoBehaviour 
+{
+
+    [SerializeField]
+    private string lookForTag;
+
+    [SerializeField]
+    private UnityEvent collisionEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == lookForTag)
+        {
+            collisionEvent.Invoke();
+        }
+    }
+}
--- Deltion_Y2P4/Assets/Scripts/Puzzles/PZ_RollTheBall.cs
+++ Deltion_Y2P4/Assets/Scripts/Puzzles/PZ_RollTheBall.cs
@@ -6,6 +6,48 @@ public class PZ_RollTheBall : MonoBehaviour
     [SerializeField]
     private Animator arenaAnim;
 
+    [SerializeField]
+    private GameObject ball;
+    private GameObject activeBall;
+
+    [SerializeField]
+    private Transform ballStart;
+
+    [SerializeField]
+    private float ballTpInvinsibility = 1f;
+    private bool ballCanTp = true;
+    private float ballTpCooldown;
+
+    private void Awake()
+    {
+        activeBall = Instantiate(ball, ballStart.position, Quaternion.identity, arenaAnim.transform);
+    }
+
+    private void Update()
+    {
+        if (!ballCanTp)
+        {
+            if (ballTpCooldown > 0)
+            {
+                ballTpCooldown -= Time.deltaTime;
+            }
+            else
+            {
+                ballCanTp = true;
+            }
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            RotateArenaLeft();
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            RotateArenaRight();
+        }
+    }
+
     public void RotateArenaRight()
     {
         arenaAnim.SetTrigger("RotateRight");
@@ -14,5 +56,21 @@ public class PZ_RollTheBall : MonoBehaviour
     public void RotateArenaLeft()
     {
         arenaAnim.SetTrigger("RotateLeft");
+    }
+
+    public void TeleportBall(Transform destination)
+    {
+        if (ballCanTp)
+        {
+            activeBall.transform.position = destination.position;
+
+            ballTpCooldown = ballTpInvinsibility;
+            ballCanTp = false;
+        }
+    }
+
+    public void CompletePuzzle()
+    {
+        Destroy(activeBall);
     }
 }
