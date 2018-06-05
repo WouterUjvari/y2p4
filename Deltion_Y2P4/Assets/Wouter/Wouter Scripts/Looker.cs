502f107cb0af9f7dc08e023c67a1a38e001b99c7
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2018-06-05 16:43:37 +0200

    hingejoints

---
+++ Deltion_Y2P4/Assets/Wouter/Wouter Scripts/Looker.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// @robotduck 2011
+// set the object's rigidbody angular drag to a high value, like 10
+
+public class Looker : MonoBehaviour
+{
+
+    public Transform target;
+    public float force = 0.1f;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+
+        Vector3 targetDelta = target.position - transform.position;
+
+        //get the angle between transform.forward and target delta
+        float angleDiff = Vector3.Angle(transform.forward, targetDelta);
+
+        // get its cross product, which is the axis of rotation to
+        // get from one vector to the other
+        Vector3 cross = Vector3.Cross(transform.forward, targetDelta);
+
+        // apply torque along that axis according to the magnitude of the angle.
+        rb.AddTorque(cross * angleDiff * force);
+    }
+}
---
+++ Deltion_Y2P4/Assets/Wouter/Wouter Scripts/Looker2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Looker2 : MonoBehaviour
+{
+
+    public Transform target;
+
+    private void LateUpdate()
+    {
+        Vector3 relativePos = transform.position - target.position;
+        relativePos.y = 0;
+        Quaternion rotation = Quaternion.LookRotation(relativePos);
+
+        Vector3 rotationEuler = rotation.eulerAngles;
+        transform.eulerAngles = rotationEuler;
+    }
+}
