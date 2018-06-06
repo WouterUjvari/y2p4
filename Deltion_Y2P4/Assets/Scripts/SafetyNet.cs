19f1fd7a06d94d9774fef32a75c83d5ba6b82d95
Dominik Beens <dominik.beens@hotmail.nl>
2018-06-06 22:30:09 +0200

    bugfixes, added safetynet

---
+++ Deltion_Y2P4/Assets/Scripts/SafetyNet.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class SafetyNet : MonoBehaviour 
+{
+
+    [SerializeField]
+    private Transform savedItemsSpawn;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.GetComponent<Interactable>() != null)
+        {
+            Rigidbody rb = other.transform.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.velocity = Vector3.zero;
+            }
+
+            other.transform.position = savedItemsSpawn.position;
+        }
+    }
+}
