ae4ab16a25e9549b56119a5cf6c062efcc604aa3
Kevin Otten <kevin.otten@ziggo.nl>
2018-06-06 14:12:20 +0200

    Coliders op cockpit

--- Deltion_Y2P4/Assets/Kevin/WorkingOn/PuzzleAudioPlayer.cs
+++ Deltion_Y2P4/Assets/Kevin/WorkingOn/PuzzleAudioPlayer.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioManager : MonoBehaviour {
+public class PuzzleAudioPlayer : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
