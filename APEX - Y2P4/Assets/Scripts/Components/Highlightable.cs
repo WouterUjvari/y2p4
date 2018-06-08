51396e24081a258eb44c9746dd0e6ef713b6beb7
DominikBeens <SchoolPC>
2018-06-08 12:24:42 +0200

    converted project to default pipeline

---
+++ APEX - Y2P4/Assets/Scripts/Components/Highlightable.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highlightable : MonoBehaviour 
+{
+
+    [SerializeField]
+    private Material highlightMat;
+
+    [SerializeField]
+    private List<MeshRenderer> renderersToExclude = new List<MeshRenderer>();
+
+    private MeshRenderer[] renderers;
+
+	private bool isHighlighted;
+
+    private void Awake()
+    {
+        if (highlightMat == null)
+        {
+            return;
+        }
+
+        highlightMat = Instantiate(highlightMat);
+
+        renderers = GetComponentsInChildren<MeshRenderer>();
+        for (int i = 0; i < renderers.Length; i++)
+        {
+            if (!renderersToExclude.Contains(renderers[i]))
+            {
+                List<Material> materials = new List<Material>();
+                materials.AddRange(renderers[i].materials);
+
+                materials.Add(highlightMat);
+
+                renderers[i].materials = materials.ToArray();
+            }
+        }
+    }
+
+    public void Highlight()
+    {
+        if (highlightMat == null)
+        {
+            return;
+        }
+
+        highlightMat.SetFloat("_Alpha", 0.35f);
+		isHighlighted = true;
+    }
+
+    public void DeHighlight()
+    {
+        if (highlightMat == null)
+        {
+            return;
+        }
+
+        highlightMat.SetFloat("_Alpha", 0);
+		isHighlighted = false;
+    }
+}
---
+++ APEX - Y2P4/Assets/Scripts/Player/VRWalk.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class VRWalk : MonoBehaviour
+{
+
+    private SteamVR_TrackedObject trackedObj;
+    //private SteamVR_Controller.Device controller;
+
+    private SteamVR_Controller.Device controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+    [SerializeField]
+    private float moveSpeed;
+
+    private void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+        //controller = SteamVR_Controller.Input((int)trackedObj.index);
+    }
+
+    private void Update()
+    {
+        if (VRPlayerMovementManager.instance.movementType == VRPlayerMovementManager.MovementType.TouchpadWalking)
+        {
+            HandleMovement();
+        }
+    }
+
+    private void HandleMovement()
+    {
+        if (controller.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            Vector2 touchpad = controller.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad);
+
+            float speed = Time.deltaTime * moveSpeed;
+
+			Vector3 nextPos = (VRPlayerMovementManager.instance.headTransform.transform.right * (touchpad.x * speed) + VRPlayerMovementManager.instance.headTransform.transform.forward * (touchpad.y * speed)) * Time.deltaTime;
+			nextPos.y = 0;
+			VRPlayerMovementManager.instance.cameraRigTransform.transform.position += nextPos;
+		}
+    }
+}
