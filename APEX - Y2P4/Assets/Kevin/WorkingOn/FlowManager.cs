5a33943dd919ddb418e2d375005a448e2158379d
Kevin Otten <kevin.otten@ziggo.nl>
2018-06-12 16:13:29 +0200

    AudioPlayer weer terug en op de deuren in main scene

---
+++ APEX - Y2P4/Assets/Kevin/WorkingOn/FlowManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowManager : MonoBehaviour {
+
+	public delegate void PuzzleEvents();
+
+	private FlowManager instance;
+	private PuzzleEvents puzzleEvent;
+
+	private int currentEvent;
+	public int eventAmount;
+
+	public List<PuzzleEvents> PuzzleEventList = new List<PuzzleEvents>();
+	void Start () 
+	{
+		if(instance == null)
+		{
+			instance = this;
+		}
+		else if(instance != this)
+		{
+			Destroy(gameObject);
+		}
+
+		for (int i = 0; i < eventAmount; i++)
+		{
+			PuzzleEventList.Add(puzzleEvent);
+		}
+	}
+
+	public void nextEvent()
+	{
+		PuzzleEventList[currentEvent]();
+		eventAmount ++;
+	}
+}
