4c20ce795ce5b136cb779033c5d9c5b93e76e7ed
DominikBeens <SchoolPC>
2018-06-12 10:05:02 +0200

    scene tweaks

---
+++ APEX - Y2P4/Assets/Scripts/Components/FollowTarget.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour {
+
+    [SerializeField]
+    private Transform target;
+
+    private void Update()
+    {
+        transform.position = target.position;
+        transform.rotation = target.rotation;
+    }
+}
