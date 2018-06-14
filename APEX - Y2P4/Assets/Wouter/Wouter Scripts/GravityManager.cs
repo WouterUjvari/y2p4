92008ef60a9d3c4f31b2310c449b50d25c0d6f04
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2018-06-14 10:18:44 +0200

    gravSwitcher

---
+++ APEX - Y2P4/Assets/Wouter/Wouter Scripts/GravityManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityManager : MonoBehaviour {
+
+
+    public float ygravity;
+    public float ydefault;
+    public Animator anim;
+
+    public bool dograv = true;
+    
+
+    
+
+    void Start()
+    {
+        ygravity = Physics.gravity.y;
+        dograv = true;
+    }
+
+    void Update()
+    {
+        anim.SetBool("On", dograv);
+
+        Physics.gravity = new Vector3(0, ygravity, 0);
+    }
+
+    public void Switchgrav()
+    {
+        dograv = !dograv;
+    }
+}
