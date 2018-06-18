8e9f972b617a593bbc7c520479022db87683a747
Kevin Otten <kevin.otten@ziggo.nl>
2018-06-18 15:51:58 +0200

    small fade in Waking up effect

--- APEX - Y2P4/Assets/Kevin/WorkingOn/DebugInteraction.cs
+++ APEX - Y2P4/Assets/Kevin/WorkingOn/DebugInteraction.cs
@@ -6,8 +6,25 @@ using UnityEngine.Events;
 public class DebugInteraction : MonoBehaviour {
 
 	public UnityEvent interactEvent;
+	public bool keepGoing;
+	public float timer;
 
-	void Start () {
+	void Start () 
+	{
+		StartCoroutine(ActivateTimer(0));
+	}
+
+	public void InvokeInteract()
+	{
 		interactEvent.Invoke();
+		if(keepGoing == true)
+		{
+			StartCoroutine(ActivateTimer(timer));
+		}
+	}
+	public IEnumerator ActivateTimer(float timer)
+	{
+		yield return new WaitForSeconds(timer);
+		InvokeInteract();
 	}
 }
---
+++ APEX - Y2P4/Assets/Kevin/WorkingOn/FadeInOut.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeInOut : MonoBehaviour {
+
+	public Animator fadeInOut;
+	// Use this for initialization
+	void Start () 
+	{
+		toggleFade();	
+	}
+	
+	public void toggleFade()
+	{
+		fadeInOut.SetTrigger("Fade");
+	}
+}
