c5b525df8ff7100029d79a6621d177a355b19592
Dominik Beens <dominik.beens@hotmail.nl>
2018-06-21 15:24:57 +0200

    implemented fire extinguisher, added keymanager

---
+++ APEX - Y2P4/Assets/Scripts/KeyManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class KeyManager : MonoBehaviour 
+{
+
+    private int keysRequired;
+    private int currentKeys;
+
+    private bool canOpenLastDoor;
+
+    [SerializeField]
+    private ObjectSnapper keySnapper;
+    [SerializeField]
+    private TextMeshProUGUI keyHolderDisplayText;
+    [SerializeField]
+    private Door doorToOpen;
+
+    private void Awake()
+    {
+        keysRequired = keySnapper.snapSpots.Count;
+    }
+
+    public void UseKey()
+    {
+        currentKeys++;
+
+        if (currentKeys >= keysRequired)
+        {
+            AllKeysAquired();
+        }
+    }
+
+    private void AllKeysAquired()
+    {
+        keyHolderDisplayText.text = "STATUS:\nONLINE";
+        canOpenLastDoor = true;
+    }
+
+    public void OpenDoor()
+    {
+        if (!canOpenLastDoor)
+        {
+            return;
+        }
+
+        keyHolderDisplayText.text = "ACCESS\nGRANTED";
+        doorToOpen.OpenCloseDoor();
+    }
+}
