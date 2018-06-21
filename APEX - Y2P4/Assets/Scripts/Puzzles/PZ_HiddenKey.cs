303b2a81d4eabfae7d98a545ed8d8f421037f66c
Dominik Beens <dominik.beens@hotmail.nl>
2018-06-21 16:17:48 +0200

    hidden key puzzle should be done, only need to implement it

---
+++ APEX - Y2P4/Assets/Scripts/Puzzles/PZ_HiddenKey.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PZ_HiddenKey : MonoBehaviour 
+{
+
+    [SerializeField]
+    private Collider hiddenKeyCollider;
+
+    [SerializeField]
+    private List<GameObject> hints = new List<GameObject>();
+
+    private void Awake()
+    {
+        hiddenKeyCollider.enabled = false;
+    }
+
+    public void StartPuzzle()
+    {
+        hiddenKeyCollider.enabled = true;
+
+        for (int i = 0; i < hints.Count; i++)
+        {
+            hints[i].SetActive(true);
+        }
+    }
+
+    public void CompletePuzzle()
+    {
+        hiddenKeyCollider.enabled = false;
+    }
+}
