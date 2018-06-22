5de71e59138b6c10538e4fc908a9fd810c0234f4
Kevin Otten <kevin.otten@ziggo.nl>
2018-06-22 16:03:40 +0200

    restrictions

---
+++ APEX - Y2P4/Assets/Kevin/WorkingOn/IRLMovementRestrictions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IRLMovementRestrictions : MonoBehaviour {
+
+	public void OnTriggerEnter(Collider other)
+	{
+		if(other.transform.gameObject.layer == 14)
+		{
+			gameObject.GetComponent<Rigidbody>().velocity = Vector3.back * Time.deltaTime;
+		}
+	}
+}
