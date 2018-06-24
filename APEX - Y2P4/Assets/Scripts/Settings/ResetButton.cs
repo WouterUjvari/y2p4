763fa9d121d3e4b235bd19990fddb6cd38891bb2
Kevin Otten <kevin.otten@ziggo.nl>
2018-06-24 16:42:57 +0200

    reset button

---
+++ APEX - Y2P4/Assets/Scripts/Settings/ResetButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetButton : MonoBehaviour {
+
+	public void ResetGame()
+	{
+		SceneManager.LoadScene(0);
+	}
+}
