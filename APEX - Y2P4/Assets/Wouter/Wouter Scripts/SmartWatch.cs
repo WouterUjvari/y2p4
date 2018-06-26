19b079bed2d36306d9e162fe1d2c7ef0d1841e5a
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2018-06-26 15:46:56 +0200

    smartwatch tips not idea if it works

--- APEX - Y2P4/Assets/Wouter/Wouter Scripts/SmartWatch.cs
+++ APEX - Y2P4/Assets/Wouter/Wouter Scripts/SmartWatch.cs
@@ -1,19 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class SmartWatch : MonoBehaviour {
+public class SmartWatch : MonoBehaviour
+{
 
     public bool enableWaypoint;
     public GameObject waypointArrow;
     public Transform target;
+    public bool tipsOn;
+    public GameObject tipElement;
+    
+    public List<GameObject> listOfTips = new List<GameObject>();
+    
+
 
     void FixedUpdate()
     {
         waypointArrow.SetActive(enableWaypoint);
-        if(enableWaypoint)
+        if (enableWaypoint)
         {
             waypointArrow.transform.LookAt(target);
         }
     }
+
+    public void ToggleTips()
+    {
+        tipsOn = !tipsOn;
+        tipElement.SetActive(tipsOn); 
+
+        listOfTips[0].SetActive(false);
+        listOfTips[1].SetActive(false);
+        listOfTips[2].SetActive(false);
+        listOfTips[3].SetActive(false);
+        listOfTips[4].SetActive(false);
+        listOfTips[5].SetActive(false);
+
+        listOfTips[FlowManager.instance.currentPuzzle].SetActive(true);
+    }
+
+
 }
