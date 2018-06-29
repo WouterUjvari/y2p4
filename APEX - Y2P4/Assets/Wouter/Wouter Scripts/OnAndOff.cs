d6ef3e0c3a43c5635ff4231d1915542a91cb85be
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2018-06-29 15:49:34 +0200

    better tutorial

---
+++ APEX - Y2P4/Assets/Wouter/Wouter Scripts/OnAndOff.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnAndOff : MonoBehaviour {
+
+    public List<GameObject> obs = new List<GameObject>();
+
+    public void TurnOffObjects()
+    {
+        for (int i = 0; i < obs.Count; i++)
+        {
+            if (obs[i] != null)
+            {
+                obs[i].SetActive(false);
+            }
+
+        }
+    }
+
+    public void TurnOnObjects()
+    {
+        for (int i = 0; i < obs.Count; i++)
+        {
+            if (obs[i] != null)
+            {
+                obs[i].SetActive(true);
+            }
+        }
+    }
+}
