4fc3ed77d18b46d3507c0004c47ba2a0fcacf897
Dominik Beens <dominik.beens@hotmail.nl>
2018-07-02 19:26:23 +0200

    small bugfixes

---
+++ APEX - Y2P4/Assets/Scripts/Editor/CustomClipListInspector.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ClipList))]
+public class CustomClipListInspector : Editor 
+{
+
+    public ClipList clipList;
+
+    private void OnEnable()
+    {
+        clipList = (ClipList)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        GUILayout.Space(40);
+
+        #region Save Asset Button
+        GUILayout.BeginHorizontal();
+
+        if (GUILayout.Button("Save Asset"))
+        {
+            SaveAssetsDirty();
+        }
+
+        GUILayout.EndHorizontal();
+        #endregion
+    }
+
+    private void SaveAssetsDirty()
+    {
+        EditorUtility.SetDirty(target);
+        AssetDatabase.SaveAssets();
+    }
+}
